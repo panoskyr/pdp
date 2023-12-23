98267d3dc0d53a23f654f5eb4852a0cb095f2373
kyrmpatsos-tetractys <141019133+kyrmpatsos-tetractys@users.noreply.github.com>
2023-12-23 12:57:28 +0200

    initialize

---
+++ keygen.py
@@ -0,0 +1,26 @@
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+def keygen(key_size):
+    private_key=rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=key_size
+    )
+    private_key_readable=private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+    
+
+    public_key=private_key.public_key()
+    public_key_readable=public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+
+    return private_key_readable, public_key_readable
+
+
+
+
+
