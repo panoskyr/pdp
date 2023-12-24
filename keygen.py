4e1d97230c55af65b89e17605de2c4098ef320ca
kyrmpatsos-tetractys <141019133+kyrmpatsos-tetractys@users.noreply.github.com>
2023-12-24 12:22:10 +0200

    found n,p,q and g

--- keygen.py
+++ keygen.py
@@ -1,5 +1,7 @@
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
+import os
+
 def keygen(key_size):
     private_key=rsa.generate_private_key(
         public_exponent=65537,
@@ -17,8 +19,32 @@ def keygen(key_size):
         encoding=serialization.Encoding.PEM,
         format=serialization.PublicFormat.SubjectPublicKeyInfo
     )
+    # print(public_key.public_numbers().n)
 
-    return private_key_readable, public_key_readable
+    p=private_key.private_numbers().p
+    q=private_key.private_numbers().q
+    N=public_key.public_numbers().n
+    # print(int.from_bytes(os.urandom(16), byteorder='big'))
+    return (N, p, q)
+
+    
+keygen(512)
+def is_ok_number(a, p, q):
+    aModp=a % p
+
+    if (
+        aModp==0 or aModp==1 or aModp==(p-1) 
+    ):
+        return False
+    
+    aModq=a % q 
+
+    if (
+        aModq==0 or aModq==1 or aModq==(q-1)
+    ):
+        return False
+    return True
+
 
 
 
