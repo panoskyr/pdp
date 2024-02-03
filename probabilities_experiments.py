8f4a5b19085bad0c99027d5a1682b49f240702dc
kyrmpatsos-tetractys <141019133+kyrmpatsos-tetractys@users.noreply.github.com>
2024-02-03 13:54:27 +0200

    asdf

--- probabilities_experiments.py
+++ probabilities_experiments.py
@@ -15,7 +15,7 @@ def plot_probability_graph(n_values, c_percentages):
         probabilities=[]
         for c_percentage in c_percentages:
             c=(c_percentage/100) * n
-            t=0.01*n
+            t=0.05*n
             probabilities.append(calculate_probability(n,c,t))
         ax.plot(c_percentages, probabilities, label=f'n={n}')
     
@@ -25,12 +25,45 @@ def plot_probability_graph(n_values, c_percentages):
     ax.legend()
     ax.grid(True)
     
-    fig.savefig(fname="Probability of detection for different size of deleted blocks more n.jpg")
+    fig.savefig(fname="p-detect-5-percent.jpg")
 
 
 # Example usage
-n_values = np.linspace(10000, 20000, 9)  # Adjust the range as needed
-c_values = np.linspace(start=10, stop=50, num=5)
+# n_values = np.linspace(1000, 10000, 9)  # Adjust the range as needed
+# c_values = np.linspace(start=0, stop=20, num=20)
 
 
-plot_probability_graph(n_values, c_values)
+# plot_probability_graph(n_values, c_values)
+    
+def a():
+    n=1000
+    t=0.05*n
+    c=380
+    print(calculate_probability(n,c,t))
+
+
+def plot_fixed_probability():
+    n_values=np.linspace(1000,100000,20)
+    c_values=np.linspace(50,400,7)
+    fig,ax=plt.subplots()
+
+    for n in n_values:
+        probabilities=[]
+        t=0.05*n 
+        for c in c_values:
+            probabilities.append(calculate_probability(n=n,c=c, t=t))
+        ax.plot(c_values,probabilities)
+    
+    ax.set_title("P(detection)" )
+    ax.set_xlabel("Number of queried blocks (c) as a % of n")
+    ax.set_ylabel("Probability of detection")
+    ax.legend()
+    ax.grid(True)
+    
+    fig.savefig(fname="p-fixed-5-percent.jpg")
+
+plot_fixed_probability()
+
+
+
+
